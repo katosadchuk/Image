0fb35a959dbec1a529bc826986b5f4bed7858ca1
Kateryna Osadchuk <tug01821@temple.edu>
2018-08-29 21:05:28 -0400

    reads in image info, updated POM

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.imaging.ImageReadException;
 
 public class Main {
 
+	//error when selecting directory
 	
-	
-	public static void parseFiles(File file){
+	public static void parseFiles(File file) throws ImageReadException, IOException{
+		
 		if(file.isDirectory()){
 			File dir = new File(file.getAbsolutePath());
 			File[] listOfFiles = dir.listFiles();
+			
 			for(File f : listOfFiles){
 				if(f.isFile()){
-					Metadata.readInfo(f);
+					Metadata.getInfo(f);
+					System.out.println(f.getName());
 				} else {
 					parseFiles(f);
 				}
 			}
 		} else {
-			Metadata.readInfo(file);
+			Metadata.getInfo(file);
+			System.out.println(file.getName());
 		}
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ImageReadException, IOException {
 		FileIndicator fi = new FileIndicator();
 		parseFiles(fi.selectedFile);
 		
--- src/Metadata.java
+++ src/Metadata.java
@@ -1,25 +1,20 @@
 import java.io.File;
 import java.io.IOException;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.lang.GeoLocation;
-import com.drew.metadata.exif.GpsDirectory;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.common.ImageMetadata;
+import org.apache.commons.imaging.*;
 
 public class Metadata {
-
 	
-	public static void readInfo(File file){
-		String name = file.getName();
-		try {
-			com.drew.metadata.Metadata metadata = ImageMetadataReader.readMetadata(file);
-			System.out.println(metadata);
-		} catch (ImageProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void getInfo(File file) throws ImageReadException, IOException{
+		ImageInfo imageInfo = Imaging.getImageInfo(file);
+		imageInfo.dump();
+	}
+	
+	public static void main(String args[]) throws ImageReadException, IOException{
+		FileIndicator fi = new FileIndicator();
+		File file = fi.selectedFile;
+		getInfo(file);
 	}
 }
